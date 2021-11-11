51749f1b8150ada54af261aa7f287ff54a8193a1
Oleg Kirsanov <olegkirsanov22@gmail.com>
2021-11-11 18:18:46 +0300

    Create tab bar and items.

---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,28 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Oleg Kirsanov on 06.11.2021.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
