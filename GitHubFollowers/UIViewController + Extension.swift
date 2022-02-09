89820b15216c557ed579665fab0a48388251a6e3
Oleg <olegkirsanov22@gmail.com>
2022-02-09 23:16:55 +0300

    Create UIViewController extension for alerts.

--- GitHubFollowers/UIViewController + Extension.swift
+++ GitHubFollowers/UIViewController + Extension.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlert(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
