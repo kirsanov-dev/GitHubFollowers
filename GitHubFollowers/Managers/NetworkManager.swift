9b7eb25b441f13ccf3efe9056b9838598c4e4b67
Oleg <olegkirsanov22@gmail.com>
2022-02-26 21:19:14 +0300

    Create network call and refactor User model code.

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -51,8 +51,43 @@ class NetworkManager {
                 completion(.failure(.invalidData))
             }
         }
-        
         task.resume()
+    }
+    
+    func getUserInfo(for username: String, completion: @escaping (Result<User, GFError>) -> Void) {
+        let endpoint = baseURL + "\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidRequest))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
+            if let _ = error {
+                completion(.failure(.unableToCompleteRequest))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        task.resume()
     }
 }
--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -9,9 +9,10 @@ import Foundation
 
 struct User: Codable {
     let login: String
-    let name: String?
-    let location: String?
-    let bio: String?
+    let avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
     let publicRepos: Int
     let publicGists: Int
     let htmlUrl: String
